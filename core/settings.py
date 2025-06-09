7a99acbdfe018919a34908252e65763b35895db6
Oleksandr Zaiets <mykola.oleksandr.zaiets@gmail.com>
2025-06-09 17:22:21 +0300

    Implement API Documentation
    Added Swagger and ReDoc documentation

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "django_filters",
+    "drf_yasg",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
     "users",
